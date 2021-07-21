f53352cd80210771779701974dcc7fd62529bf0c
songk1992-home <zuppiy@naver.com>
2021-07-22 00:45:48 +0900

    JSON 파일 내의 특이점에 의한 오류 수정 및 CSS 호버 효과 최적화

--- src/page/bible-view.ts
+++ src/page/bible-view.ts
@@ -21,63 +21,72 @@ const template: string = `
 export default class BibleView extends View {
 
     private _bStore : BStore;
+    private _abbrev : string; // TODO 추후 수정
+    private _name : string; // TODO 추후 수정
+    private _book : any; // TODO 추후 수정
+    private _chapter : any; // TODO 추후 수정
 
     constructor(containerId: string, bStore: BStore){
         super(containerId, template);
         this._bStore = bStore;
+        this._abbrev = '';
+        this._name = '';
+        this._book = null;
+        this._chapter = null;
+
     }
 
     render = async (): Promise<void> => {
-        const api = new BibleApi();
-        const {abbrev, name, book} = await api.getData();
+
+        if(this._book == null)
+        {
+            const api = new BibleApi();
+            const {abbrev, name, book} = await api.getData();
+
+            this._abbrev = abbrev;
+            this._name = name;
+            this._book = book;
+        }
 
         // 신약 마태복음 추후 수정
-        this._bStore.currentBook = this.findRandomNo(book.length);
-        if(book[this._bStore.currentBook] == NaN || book[this._bStore.currentBook] == undefined) 
+        let tempNo : number = Math.floor(Math.random() * this._book.length);
+        
+        const {ch_name, chapter} = this._book[tempNo];
+        if(ch_name == undefined || chapter == undefined)
         {
-            this.render();
-            return;
+            return this.render();
         }
+        this._chapter = chapter;
 
-        const {ch_name, chapter} = book[this._bStore.currentBook];
-        this._bStore.currentChapter = this.findRandomNo(chapter.length);
-        if(chapter[this._bStore.currentChapter] == NaN || chapter[this._bStore.currentChapter] == undefined) 
+        tempNo = Math.floor(Math.random() * this._chapter.length);
+        if(typeof(this._chapter.num) == "string")
         {
-            this.render();
-            return;
+            return this.render();
         }
-
-        const {ch_no, verse} = chapter[this._bStore.currentChapter];
-        this._bStore._currentVerse = this.findRandomNo(verse.length);
-        if(verse[this._bStore.currentVerse] == NaN || verse[this._bStore.currentVerse] == undefined) 
+        // console.log(this._chapter);
+        // console.log(this._chapter[tempNo]);
+        // console.log(tempNo);
+        const {ch_no, verse} = this._chapter[tempNo];
+        if(ch_no == undefined || verse == undefined)
         {
-            this.render();
-            return;
+            return this.render();
         }
+        
         const {verse_no, verse_text} = verse[this._bStore.currentVerse];
+        if(verse_no == undefined || verse_text == undefined)
+        {
+            return this.render();
+        }
 
-        this.setTemplateData('abbrev', abbrev);
-        this.setTemplateData('name', name);
+
+        this.setTemplateData('abbrev', this._abbrev);
+        this.setTemplateData('name', this._name);
         this.setTemplateData('ch_name', ch_name);
         this.setTemplateData('ch_no', ch_no);
         this.setTemplateData('verse_no', verse_no);
         this.setTemplateData('verse_text', verse_text);
 
         this.updateView();
-    }
-
-    private findRandomNo(curLen:number)
-    {
-        let tempNo :number = Math.floor(Math.random() * curLen);
-        if(tempNo == null)
-        {
-            tempNo = 0;
-        }
-        if(tempNo == NaN)
-        {
-            tempNo = 0;
-        }
-        return tempNo;
     }
     
 }
