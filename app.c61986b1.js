9a532356479cd9a4d7cd20cfb1851dc5b114dcfd
songk1992-home <zuppiy@naver.com>
2021-07-25 16:23:38 +0900

    Updates

--- app.c61986b1.js
+++ app.c61986b1.js
@@ -240,7 +240,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var view_1 = __importDefault(require("../core/view"));
 
-var template = "     \n<div class=\"my_container\">\n    <div class=\"my_text\">\n        <a href=\"#/bible/\"> \uC131\uACBDApi</a>\n        <a href=\"#\"> N\uC0AC \uD074\uB860 \uCF54\uB529</a>\n    </div>\n</div>\n";
+var template = "     \n<div class=\"my_container\">\n    <div class=\"my_text\">\n        <a href=\"#/bible/\"> \uC131\uACBDApi</a>\n    </div>\n</div>\n";
 
 var IndexView =
 /** @class */
@@ -260,14 +260,196 @@ function (_super) {
 }(view_1.default);
 
 exports.default = IndexView;
-},{"../core/view":"core/view.ts"}],"config.ts":[function(require,module,exports) {
+},{"../core/view":"core/view.ts"}],"core/api/api.ts":[function(require,module,exports) {
+"use strict";
+
+var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : new P(function (resolve) {
+        resolve(result.value);
+      }).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+var __generator = this && this.__generator || function (thisArg, body) {
+  var _ = {
+    label: 0,
+    sent: function sent() {
+      if (t[0] & 1) throw t[1];
+      return t[1];
+    },
+    trys: [],
+    ops: []
+  },
+      f,
+      y,
+      t,
+      g;
+  return g = {
+    next: verb(0),
+    "throw": verb(1),
+    "return": verb(2)
+  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
+    return this;
+  }), g;
+
+  function verb(n) {
+    return function (v) {
+      return step([n, v]);
+    };
+  }
+
+  function step(op) {
+    if (f) throw new TypeError("Generator is already executing.");
+
+    while (_) {
+      try {
+        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+        if (y = 0, t) op = [op[0] & 2, t.value];
+
+        switch (op[0]) {
+          case 0:
+          case 1:
+            t = op;
+            break;
+
+          case 4:
+            _.label++;
+            return {
+              value: op[1],
+              done: false
+            };
+
+          case 5:
+            _.label++;
+            y = op[1];
+            op = [0];
+            continue;
+
+          case 7:
+            op = _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+
+          default:
+            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+              _ = 0;
+              continue;
+            }
+
+            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
+              _.label = op[1];
+              break;
+            }
+
+            if (op[0] === 6 && _.label < t[1]) {
+              _.label = t[1];
+              t = op;
+              break;
+            }
+
+            if (t && _.label < t[2]) {
+              _.label = t[2];
+
+              _.ops.push(op);
+
+              break;
+            }
+
+            if (t[2]) _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+        }
+
+        op = body.call(thisArg, _);
+      } catch (e) {
+        op = [6, e];
+        y = 0;
+      } finally {
+        f = t = 0;
+      }
+    }
+
+    if (op[0] & 5) throw op[1];
+    return {
+      value: op[0] ? op[1] : void 0,
+      done: true
+    };
+  }
+};
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var Api =
+/** @class */
+function () {
+  function Api(url) {
+    this.url = url;
+  }
+
+  Api.prototype.request = function () {
+    return __awaiter(this, void 0, Promise, function () {
+      var response;
+      return __generator(this, function (_a) {
+        switch (_a.label) {
+          case 0:
+            return [4
+            /*yield*/
+            , fetch(this.url)];
+
+          case 1:
+            response = _a.sent();
+            return [4
+            /*yield*/
+            , response.json()];
+
+          case 2:
+            return [2
+            /*return*/
+            , _a.sent()];
+        }
+      });
+    });
+  };
+
+  return Api;
+}();
+
+exports.default = Api;
+},{}],"config.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.BIBLE_URL = "https://raw.githubusercontent.com/songk1992/mini_front_end_projects/master/src/data/newTestament.json";
-},{}],"core/api.ts":[function(require,module,exports) {
+},{}],"core/api/api_bible.ts":[function(require,module,exports) {
 "use strict";
 
 var __extends = this && this.__extends || function () {
@@ -431,52 +613,24 @@ var __generator = this && this.__generator || function (thisArg, body) {
   }
 };
 
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var config_1 = require("../config");
+var api_1 = __importDefault(require("./api"));
 
-var Api =
-/** @class */
-function () {
-  function Api(url) {
-    this.url = url;
-  }
-
-  Api.prototype.request = function () {
-    return __awaiter(this, void 0, Promise, function () {
-      var response;
-      return __generator(this, function (_a) {
-        switch (_a.label) {
-          case 0:
-            return [4
-            /*yield*/
-            , fetch(this.url)];
-
-          case 1:
-            response = _a.sent();
-            return [4
-            /*yield*/
-            , response.json()];
-
-          case 2:
-            return [2
-            /*return*/
-            , _a.sent()];
-        }
-      });
-    });
-  };
-
-  return Api;
-}();
-
-exports.default = Api;
+var config_1 = require("../../config");
 /**
  * 성경을 읽어오는 클래스
  * 추후 필요시 외부 URL 과 연결된 DB로 변경
  */
+
 
 var BibleApi =
 /** @class */
@@ -498,10 +652,10 @@ function (_super) {
   };
 
   return BibleApi;
-}(Api);
+}(api_1.default);
 
 exports.BibleApi = BibleApi;
-},{"../config":"config.ts"}],"page/bible-view.ts":[function(require,module,exports) {
+},{"./api":"core/api/api.ts","../../config":"config.ts"}],"page/bible-view.ts":[function(require,module,exports) {
 "use strict";
 
 var __extends = this && this.__extends || function () {
@@ -677,7 +831,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var view_1 = __importDefault(require("../core/view"));
 
-var api_1 = require("../core/api");
+var api_bible_1 = require("../core/api/api_bible");
 
 var template = "\n<div class=\"bible-menu\">\n<p>\uC791\uC5C5\uC911</p>\n<p>{{__abbrev__}}</p>\n<p>{{__name__}}</p>\n<a href=\"#\"> \uB4A4\uB85C \uAC00\uAE30</a>\n</div>\n\n<div class=\"bible_bg\">\n    <div class=\"bible_text\">\n        <p class=\"grow\">{{__ch_name__}} {{__ch_no__}} - {{__verse_no__}}</p>\n        <p class=\"grow\">{{__verse_text__}}</p>\n    </div>\n</div>\n";
 
@@ -699,7 +853,7 @@ function (_super) {
               if (!(this._book == null)) return [3
               /*break*/
               , 2];
-              api = new api_1.BibleApi();
+              api = new api_bible_1.BibleApi();
               return [4
               /*yield*/
               , api.getData()];
@@ -773,7 +927,7 @@ function (_super) {
 }(view_1.default);
 
 exports.default = BibleView;
-},{"../core/view":"core/view.ts","../core/api":"core/api.ts"}],"page/index.ts":[function(require,module,exports) {
+},{"../core/view":"core/view.ts","../core/api/api_bible":"core/api/api_bible.ts"}],"page/index.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -904,7 +1058,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62969" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57956" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
